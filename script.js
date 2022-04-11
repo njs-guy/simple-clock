00e3c676d9cfd1265b84c19ceac54d7e99ab1f5d
Nick Shockey <shockeynj@gmail.com>
2022-04-10 22:05:02 -0400

    Clock now displays in AM/PM format
    Also cleaned up the code a bit

--- script.js
+++ script.js
@@ -1,26 +1,60 @@
- function setClock(){
+// Sets the clock displayed on screen
+
+// Main function to set time
+function setClock(){
 	 const clock = document.getElementById("clock");
 	 const clockText = document.getElementById("clock-text");
 
+	 let twelveHrClock = true;
 	 let currentTime = new Date();
 	 let currentHour = currentTime.getHours();
 	 let currentMinute = currentTime.getMinutes();
 	 let currentSecond = currentTime.getSeconds();
 
 	 let timeArr = [leadingZero(currentHour), leadingZero(currentMinute), leadingZero(currentSecond)];
-	 console.log(timeArr);
 
-	 let timeText  = timeArr[0] + " : " + timeArr[1] + " : " + timeArr[2];
-	 clockText.innerHTML = timeText;
+	 let timeText = "";
+
+	 if(twelveHrClock){
+		 timeText = convertTo12Hr(timeArr);
+	 } else {
+		 timeText = createClockOutput(timeArr);
+	 }
+
+	clockText.innerHTML = timeText;
 }
 
+// Creates leading zeroes (ex: 14:05:07)
 function leadingZero(x){
-	if(x>=0 && x<=10 ){
+	if(x>=0 && x<=10){
 		x = "0" + String(x);
 		return x;
 	} else {
 		return String(x);
 	}
+}
+
+// Creates the text that displays the time (ex: 00:00:00)
+function createClockOutput(tArr){
+	console.log(tArr.join(":"));
+	return tArr.join(":");
+}
+
+// Converts the 24 hour time to 12 hour time (ex: 01:27:45 PM)
+function convertTo12Hr(tArr) {
+	h = parseInt(tArr[0]);
+	output = "";
+
+	if (h >= 13){
+		h -= 12;
+		output = createClockOutput([h,tArr[1],tArr[2]]);
+		output += " PM";
+	} else {
+		output = createClockOutput(tArr);
+		output += " AM";
+	}
+
+	return output;
 }
 
 setClock(); // sets clock as soon as the page loads
