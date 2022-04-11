aa86a1c5dc45a91a0ed0ec08487d1eed62201cf5
Nick Shockey <shockeynj@gmail.com>
2022-04-10 20:19:58 -0400

    now displays time

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// script.js 
+ function setClock(){
+	 const clock = document.getElementById("clock");
+	 let currentTime = new Date();
+	 let currentHour = currentTime.getHours();
+	 let currentMinute = currentTime.getMinutes();
+	 let currentSeconds = currentTime.getSeconds();
+	 let clockText = document.createTextNode(currentHour + " : " + currentMinute + " : " + currentSeconds);
+
+	 console.log(clockText);
+	 clock.appendChild(clockText);
+ }
+
+setClock();
