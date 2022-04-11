f9cb586299bab1f7d832300c7f8173fcf922ae42
Nick Shockey <shockeynj@gmail.com>
2022-04-10 21:09:49 -0400

    clock now displays leading zeroes

--- script.js
+++ script.js
@@ -6,9 +6,23 @@
 	 let currentHour = currentTime.getHours();
 	 let currentMinute = currentTime.getMinutes();
 	 let currentSecond = currentTime.getSeconds();
-	 let timeText  = currentHour + " : " + currentMinute + " : " + currentSecond;
+
+	 let timeArr = [leadingZero(currentHour), leadingZero(currentMinute), leadingZero(currentSecond)];
+	 console.log(timeArr);
+
+	 let timeText  = timeArr[0] + " : " + timeArr[1] + " : " + timeArr[2];
 	 clockText.innerHTML = timeText;
 }
+
+function leadingZero(x){
+	if(x>=0 && x<=10 ){
+		x = "0" + String(x);
+		return x;
+	} else {
+		return String(x);
+	}
+}
+
 setClock(); // sets clock as soon as the page loads
 
 let i = setInterval(setClock, 1000); // sets clock every second
